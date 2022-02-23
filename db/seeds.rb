458469736c0dcd154b9268adf960118566c13cb9
Sony Tamang <sony123ktm@gmail.com>
2022-02-23 13:00:06 +1100

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Nobu', address: '00 crown street', phone_number: '009900', category: 'japanese')
+Restaurant.create(name: 'Shanghai', address: '11 shanghai street', phone_number: '001100', category: 'chinese')
+Restaurant.create(name: 'Nonnas', address: '01 rome street', phone_number: '004400', category: 'italian')
+Restaurant.create(name: 'Mon Ami', address: '02 paris street', phone_number: '005500', category: 'french')
+Restaurant.create(name: 'Liefde', address: '04 dutch street', phone_number: '006600', category: 'belgian')
