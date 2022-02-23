d8e5070df903e82bf2f4659092f4a71c60b8fab8
Sony Tamang <sony123ktm@gmail.com>
2022-02-23 15:35:13 +1100

    Yelp MVP

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new(restaurant_id: params[:restaurant_id])
+    # restaurant = Restaurant.find(params[:restaurant_id])
+    # @review = restaurant.reviews.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to restaurant_path(id: @review.restaurant.id)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content, :restaurant_id)
   end
 end
