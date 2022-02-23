0825cdf9456142ff693cb6f1fe8b16d5ac2e2b15
Sony Tamang <sony123ktm@gmail.com>
2022-02-23 12:46:30 +1100

    fix review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, :rating, presence: true
-  validates :rating, inclusion: { in: 1..5 }
+  validates :rating, inclusion: 0..5
+  validates :rating, numericality: { only_integer: true }
 end
